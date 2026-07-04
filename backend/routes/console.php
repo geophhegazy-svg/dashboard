0a7fb31ae8b186a7805bc373c5d54c55a92caaf3
Mohammed Hegazy <geoph.hegazy@gmail.com>
2026-07-04 13:22:31 +0300

    bug solve

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 Schedule::command('subscriptions:sync')
-    // ->dailyAt('00:05');
-    ->everyMinute();
+    ->dailyAt('00:05');
+    // ->everyMinute();
